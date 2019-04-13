d5611cea68c416765110d48743635f2cfe2a7dd5
Chris Bay <chris@chrisbay.net>
2019-04-13 08:51:14 -0500

    Adds tests for phone-number

--- spec/phone-number.spec.js
+++ spec/phone-number.spec.js
@@ -7,6 +7,32 @@ describe("phone number validation", function(){
         expect(isValidPhoneNumber("1234567890")).toBe(true);
     });
 
-    // TODO: Add more tests
+    xit("should not accept a number with > 10 digits", function(){
+        expect(isValidPhoneNumber("12345678901")).toBe(false);
+    });
+
+    xit("should not accept a number with < 10 digits", function(){
+        expect(isValidPhoneNumber("123456789")).toBe(false);
+    });
+
+    xit("should accept a number with parts separated by single spaces", function(){
+        expect(isValidPhoneNumber("123 456 7890")).toBe(true);
+    });
+
+    xit("should accept a number with parts separated by any whitespace", function(){
+        expect(isValidPhoneNumber("123   456    7890")).toBe(true);
+    });
+
+    xit("should accept a number with parts separated by dashes", function(){
+        expect(isValidPhoneNumber("123-456-7890")).toBe(true);
+    });
+
+    xit("should accept a number with parts separated by spaces and/or dashes", function(){
+        expect(isValidPhoneNumber("123 - 456 7890")).toBe(true);
+    });
+
+    xit("should accept a number with area code in parens", function(){
+        expect(isValidPhoneNumber("(123) 456-7890")).toBe(true);
+    });
 
 });
