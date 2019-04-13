9d722c28b0f706b1fec4b747a8d7d20db3681806
Chris Bay <chris@chrisbay.net>
2019-04-13 09:56:17 -0500

    Adds zip code spec

---
+++ spec/zip-codes.spec.js
@@ -0,0 +1,30 @@
+const mod = require('../zip-codes.js');
+const isValidZipCode = mod.isValidZipCode;
+
+describe("zip code validation", function(){
+
+    it("should allow 5 digits", function(){
+        expect(isValidZipCode(63108)).toBe(true);
+    });
+
+    xit("should allow valid codes as strings", function(){
+        expect(isValidZipCode("63108")).toBe(true);
+    });
+
+    xit("should allow plus-four codes", function(){
+        expect(isValidZipCode("12201-7050")).toBe(true);
+    });
+
+    xit("should not allow non-numeric characters in 5-digit codes", function(){
+        expect(isValidZipCode("1220a")).toBe(false);
+    });
+
+    xit("should not allow non-numeric characters in plus-four codes", function(){
+        expect(isValidZipCode("12201-7a50")).toBe(false);
+    });
+
+    xit("should not allow things that aren't even close", function(){
+        expect(isValidZipCode("zucchini")).toBe(false);
+    });
+
+});
