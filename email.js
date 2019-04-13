017d3cf8279da5c2d215bbcd3564ca14da1a8f18
Chris Bay <chris@chrisbay.net>
2019-04-13 12:41:13 -0500

    Solves email exercise

--- email.js
+++ email.js
@@ -1,5 +1,5 @@
 function isValidEmail(email) {
-    
+    return email.search(/^[a-z0-9]+(\.[a-z0-9]+)?@[a-z0-9-]+\.[a-z]{2,}$/i) > -1;
 };
 
 module.exports = { isValidEmail };
--- spec/email.spec.js
+++ spec/email.spec.js
@@ -7,51 +7,51 @@ describe("email validation", function(){
         expect(isValidEmail("launchcode.org")).toBe(false);
     });
 
-    xit("should require local part", function(){
+    it("should require local part", function(){
         expect(isValidEmail("@launchcode.org")).toBe(false);
     });
 
-    xit("should require domain", function(){
+    it("should require domain", function(){
         expect(isValidEmail("chris@")).toBe(false);
     });
 
-    xit("should require tld", function(){
+    it("should require tld", function(){
         expect(isValidEmail("chris@launchcode")).toBe(false);
     });
 
-    xit("should allow regular emails", function(){
+    it("should allow regular emails", function(){
         expect(isValidEmail("chris@launchcode.org")).toBe(true);
     });
 
-    xit("should allow . within local part", function(){
+    it("should allow . within local part", function(){
         expect(isValidEmail("chris.bay@launchcode.org")).toBe(true);
     });
 
-    xit("should not allow . at beginning of local part", function(){
+    it("should not allow . at beginning of local part", function(){
         expect(isValidEmail(".bay@launchcode.org")).toBe(false);
     });
 
-    xit("should not allow . at end of local part", function(){
+    it("should not allow . at end of local part", function(){
         expect(isValidEmail("chris.@launchcode.org")).toBe(false);
     });
 
-    xit("should not allow symbols other than . in local part", function(){
+    it("should not allow symbols other than . in local part", function(){
         expect(isValidEmail("chris!bay@launchcode.org")).toBe(false);
     });
 
-    xit("should not allow whitespace in local part", function(){
+    it("should not allow whitespace in local part", function(){
         expect(isValidEmail("chris bay@launchcode.org")).toBe(false);
     });
 
-    xit("should not allow whitespace in domain", function(){
+    it("should not allow whitespace in domain", function(){
         expect(isValidEmail("chris@launch code.org")).toBe(false);
     });
 
-    xit("should not allow whitespace in tld", function(){
+    it("should not allow whitespace in tld", function(){
         expect(isValidEmail("chris@launchcode. org")).toBe(false);
     });
 
-    xit("should allow - in domain", function(){
+    it("should allow - in domain", function(){
         expect(isValidEmail("chris@launch-code. org")).toBe(false);
     });
 });
