8e71d48480d20ca5ae9f778f7db96c6f27283ae5
Chris Bay <chris@chrisbay.net>
2019-04-13 10:05:21 -0500

    Adds zip codes solution

--- spec/zip-codes.spec.js
+++ spec/zip-codes.spec.js
@@ -7,27 +7,27 @@ describe("zip code validation", function(){
         expect(isValidZipCode("63108")).toBe(true);
     });
 
-    xit("should allow plus-four codes", function(){
+    it("should allow plus-four codes", function(){
         expect(isValidZipCode("12201-7050")).toBe(true);
     });
 
-    xit("should not allow non-numeric characters in 5-digit codes", function(){
+    it("should not allow non-numeric characters in 5-digit codes", function(){
         expect(isValidZipCode("1220a")).toBe(false);
     });
 
-    xit("should not allow non-numeric characters in plus-four codes", function(){
+    it("should not allow non-numeric characters in plus-four codes", function(){
         expect(isValidZipCode("12201-7a50")).toBe(false);
     });
 
-    xit("should not allow strings properly containing zip codes", function(){
+    it("should not allow strings properly containing zip codes", function(){
         expect(isValidZipCode("LaunchCode's zip code is 63108.")).toBe(false);
     });
 
-    xit("should not allow things that aren't even close", function(){
+    it("should not allow things that aren't even close", function(){
         expect(isValidZipCode("zucchini")).toBe(false);
     });
 
-    xit("should allow 5-digit codes as numbers", function(){
+    it("should allow 5-digit codes as numbers", function(){
         expect(isValidZipCode(63108)).toBe(true);
     });
 
--- zip-codes.js
+++ zip-codes.js
@@ -1,5 +1,6 @@
 function isValidZipCode(code) {
-    
+    code = String(code);
+    return code.search(/^\d{5}(-\d{4})?$/) > -1;
 };
 
 module.exports = { isValidZipCode };
