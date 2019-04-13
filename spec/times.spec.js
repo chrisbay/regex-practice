15be5150091eba451e6574516fd3d1621f428c97
Chris Bay <chris@chrisbay.net>
2019-04-13 11:17:02 -0500

    Solves times exercise

--- spec/times.spec.js
+++ spec/times.spec.js
@@ -7,75 +7,75 @@ describe("time validation", function(){
         expect(isValidTime("13:15")).toBe(true);
     });
 
-    xit("should allow times w/ leading zero", function(){
+    it("should allow times w/ leading zero", function(){
         expect(isValidTime("03:15")).toBe(true);
     });
 
-    xit("should allow times w/o leading zero", function(){
+    it("should allow times w/o leading zero", function(){
         expect(isValidTime("3:15")).toBe(true);
     });
 
-    xit("should not allow hour values > 23", function(){
+    it("should not allow hour values > 23", function(){
         expect(isValidTime("24:00")).toBe(false);
     });
 
-    xit("should not allow minute values > 59", function(){
+    it("should not allow minute values > 59", function(){
         expect(isValidTime("12:60")).toBe(false);
     });
 
-    xit("should allow spaces around hour and minute values", function(){
+    it("should allow spaces around hour and minute values", function(){
         expect(isValidTime(" 03 : 15    ")).toBe(true);
     });
 
-    xit("should allow reasonable AM/PM values for 12-hour times", function(){
+    it("should allow reasonable AM/PM values for 12-hour times", function(){
         expect(isValidTime("12:00 PM")).toBe(true);
     });
 
-    xit("should allow reasonable AM/PM values for 12-hour times", function(){
+    it("should allow reasonable AM/PM values for 12-hour times", function(){
         expect(isValidTime("12:00 p.m.")).toBe(true);
     });
 
-    xit("should allow reasonable AM/PM values for 12-hour times", function(){
+    it("should allow reasonable AM/PM values for 12-hour times", function(){
         expect(isValidTime("12:00 pm")).toBe(true);
     });
 
-    xit("should allow reasonable AM/PM values for 12-hour times", function(){
+    it("should allow reasonable AM/PM values for 12-hour times", function(){
         expect(isValidTime("12:00p")).toBe(true);
     });
 
-    xit("should allow reasonable AM/PM values for 12-hour times", function(){
+    it("should allow reasonable AM/PM values for 12-hour times", function(){
         expect(isValidTime("12:00 p")).toBe(true);
     });
 
-    xit("should allow reasonable AM/PM values for 12-hour times", function(){
+    it("should allow reasonable AM/PM values for 12-hour times", function(){
         expect(isValidTime("12:00 AM")).toBe(true);
     });
 
-    xit("should allow reasonable AM/PM values for 12-hour times", function(){
+    it("should allow reasonable AM/PM values for 12-hour times", function(){
         expect(isValidTime("12:00 a.m.")).toBe(true);
     });
 
-    xit("should allow reasonable AM/PM values for 12-hour times", function(){
+    it("should allow reasonable AM/PM values for 12-hour times", function(){
         expect(isValidTime("12:00 am")).toBe(true);
     });
 
-    xit("should allow reasonable AM/PM values for 12-hour times", function(){
+    it("should allow reasonable AM/PM values for 12-hour times", function(){
         expect(isValidTime("12:00a")).toBe(true);
     });
 
-    xit("should allow reasonable AM/PM values for 12-hour times", function(){
+    it("should allow reasonable AM/PM values for 12-hour times", function(){
         expect(isValidTime("12:00 a")).toBe(true);
     });
 
-    xit("should not allow AM/PM values for 24-hour times", function(){
+    it("should not allow AM/PM values for 24-hour times", function(){
         expect(isValidTime("14:00 AM")).toBe(false);
     });
 
-    xit("should not allow characters for 24-hour times other than spaces, digits, and :", function(){
+    it("should not allow characters for 24-hour times other than spaces, digits, and :", function(){
         expect(isValidTime("asdf 14:00 ")).toBe(false);
     });
 
-    xit("should require :", function(){
+    it("should require :", function(){
         expect(isValidTime("12 00 AM")).toBe(false);
     });
 
--- times.js
+++ times.js
@@ -1,5 +1,17 @@
 function isValidTime(time) {
+    const results = time.match(/^\s*(?<hours>\d{1,2})\s*:\s*(?<minutes>\d{2})\s*(?<period>([ap]\.?)(m\.?)?)?\s*$/i);
     
+    if (!results) 
+        return false;
+    
+    const hours = Number(results.groups["hours"]);
+    const minutes = Number(results.groups["minutes"]);
+    const period = results.groups["period"];
+    
+    if (period && hours > 12)
+        return false; 
+
+    return minutes >= 0 && minutes < 60 && hours >= 0 && hours < 24;
 };
 
 module.exports = { isValidTime };
