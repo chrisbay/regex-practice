654bd98d767cc1e9e6b05e13eb0e5406a1d33aa5
Chris Bay <chris@chrisbay.net>
2019-04-13 09:40:44 -0500

    Solves name formatting exercise

--- name-formatting.js
+++ name-formatting.js
@@ -1,5 +1,5 @@
 function formatName(name) {
-    
+    return name.replace(/^(\w+)(\s\w*|\s\w\.)?\s(['\w-]+)$/, "$3, $1$2");
 };
 
 module.exports = { formatName };
--- spec/name-formatting.spec.js
+++ spec/name-formatting.spec.js
@@ -7,4 +7,24 @@ describe("name formatting", function(){
         expect(formatName("Chris Bay")).toBe("Bay, Chris");
     });
 
+    it("should allow hyphenated last names", function(){
+        expect(formatName("Julia Louis-Dreyfus")).toBe("Louis-Dreyfus, Julia");
+    });
+
+    it("should format 'first middle last' as 'last, first middle'", function(){
+        expect(formatName("David Foster Wallace")).toBe("Wallace, David Foster");
+    });
+
+    it("should allow middle initials", function(){
+        expect(formatName("John F. Kennedy")).toBe("Kennedy, John F.");
+    });
+
+    it("should allow single names", function(){
+        expect(formatName("Plato")).toBe("Plato");
+    });
+
+    it("should allow single-quotes in names", function(){
+        expect(formatName("Flannery O'Connor")).toBe("O'Connor, Flannery");
+    });
+
 });
