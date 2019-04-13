bf5251c4f3d7ef2288e4aca2dce8d810c9ffe487
Chris Bay <chris@chrisbay.net>
2019-04-13 11:30:46 -0500

    Solves password exercise

--- password.js
+++ password.js
@@ -1,5 +1,9 @@
 function isValidPassword(password) {
-    // Tip: Consider using multiple regular expressions
+    const isLongEnough = password.search(/^[^\s]{12,}$/) > -1;
+    const containsDigit = password.search(/\d/) > -1;
+    const containsLetter = password.search(/[a-z]/i) > -1;
+    const containsSymbol = password.search(/[^\da-zA-Z\s]/) > -1;
+    return isLongEnough && containsDigit && containsLetter && containsSymbol;
 };
 
 module.exports = { isValidPassword };
--- spec/password.spec.js
+++ spec/password.spec.js
@@ -7,23 +7,23 @@ describe("password validation", function(){
         expect(isValidPassword("1234567890a")).toBe(false);
     });
 
-    xit("should not allow spaces", function(){
+    it("should not allow spaces", function(){
         expect(isValidPassword("123456 7890ab")).toBe(false);
     });
 
-    xit("should not allow tabs", function(){
+    it("should not allow tabs", function(){
         expect(isValidPassword("123456  7890ab")).toBe(false);
     });
 
-    xit("should require at least one digit", function(){
+    it("should require at least one digit", function(){
         expect(isValidPassword("aaaaaaaaaaaa")).toBe(false);
     });
 
-    xit("should require at least one letter", function(){
+    it("should require at least one letter", function(){
         expect(isValidPassword("111111111111")).toBe(false);
     });
 
-    xit("should require at least one symbol", function(){
+    it("should require at least one symbol", function(){
         expect(isValidPassword("a1a1a1a1a1a1")).toBe(false);
     });
 
