94ab37f1c7d29d5622fb8d1463adc868e939a6f8
Chris Bay <chris@chrisbay.net>
2019-04-13 12:28:19 -0500

    Solves web-url exercise

--- spec/web-url.spec.js
+++ spec/web-url.spec.js
@@ -7,47 +7,47 @@ describe("web URL", function(){
         expect(parseWebUrl("launchcode.org")).toEqual(null);
     });
 
-    xit("should require that URLs have a TLD", function(){
+    it("should require that URLs have a TLD", function(){
         expect(parseWebUrl("http://launchcode")).toEqual(null);
     });
 
-    xit("should require that URLs have a domain", function(){
+    it("should require that URLs have a domain", function(){
         expect(parseWebUrl("http://.org")).toEqual(null);
     });
 
-    xit("should allow domains without host", function(){
+    it("should allow domains without host", function(){
         const expected = {domain: "launchcode", tld: "org", host: undefined, path: undefined};
         expect(parseWebUrl("http://launchcode.org")).toEqual(expected);
     });
 
-    xit("should allow https", function(){
+    it("should allow https", function(){
         const expected = {domain: "launchcode", tld: "org", host: undefined, path: undefined};
         expect(parseWebUrl("https://launchcode.org")).toEqualexpected
     });
 
-    xit("should allow domains with host", function(){
+    it("should allow domains with host", function(){
         const expected = {domain: "launchcode", tld: "org", host: "www", path: undefined};
         expect(parseWebUrl("http://www.launchcode.org")).toEqual(expected);
     });
 
-    xit("should only allow valid chars (letters, number, -) in host names", function(){
+    it("should only allow valid chars (letters, number, -) in host names", function(){
         expect(parseWebUrl("http://www_.launchcode.org")).toEqual(null);
     });
 
-    xit("should only allow valid chars (letters, number, -) in domain names", function(){
+    it("should only allow valid chars (letters, number, -) in domain names", function(){
         expect(parseWebUrl("http://www.launchcode_.org")).toEqual(null);
     });
 
-    xit("should only allow letters in tld names", function(){
+    it("should only allow letters in tld names", function(){
         expect(parseWebUrl("http://www.launchcode.org1")).toEqual(null);
     });
 
-    xit("should allow domains with path", function(){
+    it("should allow domains with path", function(){
         const expected = {domain: "launchcode", tld: "org", host: undefined, path: "/education"};
         expect(parseWebUrl("http://launchcode.org/education")).toEqual(expected);
     });
 
-    xit("should allow query strings in the path", function(){
+    it("should allow query strings in the path", function(){
         const expected = {domain: "launchcode", tld: "org", host: undefined, path: "/education?q=python"};
         expect(parseWebUrl("http://launchcode.org/education?q=python")).toEqual(expected);
     });
--- web-url.js
+++ web-url.js
@@ -2,6 +2,10 @@ function parseWebUrl(url) {
 
     // return an object with properties host, domain, tld, and path for valid URLs, null otherwise
 
+    const matches = url.match(/^https?:\/\/((?<host>[a-z0-9-]+)\.)?(?<domain>[a-z0-9-]+)\.(?<tld>[a-z]{2,})(?<path>\/[a-z0-9\?=]+)?$/i);
+    if (!matches)
+        return null;
+    return matches.groups;
 };
 
 module.exports = { parseWebUrl };
